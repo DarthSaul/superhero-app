6cc8d955e57398bfcb845725e0e2ff7290bf5e39
Saul Graves <graves.saul@gmail.com>
2021-02-14 16:30:45 -0500

    home page

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello, world.')
+    res.render('home')
 });
 
 app.listen(3000, () => {
