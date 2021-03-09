0f24eb94c5f850a76bc7f139c2a63cd2c5db8a6b
Saul Graves <graves.saul@gmail.com>
2021-03-08 23:20:31 -0500

    Session and Flash

--- routes/heroes.js
+++ routes/heroes.js
@@ -31,17 +31,22 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', wrapAsync(async (req, res) => {
     const hero = await Hero.findById(req.params.id);
-    res.render('heroes/show', { hero, errorMsg: null })
+    if (!hero) {
+        req.flash("error", `Sorry, cannot find a hero profile under the ID: ${req.params.id} `)
+        return res.redirect('/heroes')
+    }
+    res.render('heroes/show', { hero })
 }));
 
-router.post('/', validateHero, wrapAsync( async (req, res) => {
+router.post('/', wrapAsync( async (req, res) => {
     const newHero = new Hero(req.body.hero);
     try {
         const hero = await newHero.save()
+        req.flash("success", "New hero added to the database!")
         res.redirect(`/heroes/${hero._id}`);
     } catch (error) {
-        const msg = error;
-        throw new ExpressError(msg, 400)
+        req.flash("error", "Whoops, something went wrong!")
+        res.redirect('/heroes/new')
     };
 }));
 
