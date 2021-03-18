3dedee9c2b9ec137e6e65a64f74dcb16bbc1fc1f
Saul Graves <graves.saul@gmail.com>
2021-03-18 18:45:25 -0400

    Update passportLocalMongoose error messages

--- models/user.js
+++ models/user.js
@@ -10,15 +10,20 @@ const userSchema = new Schema({
     }
 })
 
-userSchema.plugin(passportLocalMongoose);
+const options = {
+    errorMessages: {
+        UserExistsError: 'The username provided is already registered. Please try logging in, or use another username.'
+    }
+};
+userSchema.plugin(passportLocalMongoose, options);
 
-// "unique" is not a validator, doesn't accept error msg on schema
-// Set custom error message for unique email below
+// // "unique" is NOT a validator, doesn't accept error msg on schema
+// // Set custom error message for unique email below
 userSchema.post('save', (error, doc, next) => {
     if (error.name === 'MongoError' && error.code === 11000) {
-      next(new Error('Email is already associated with an account.'));
+        next(new Error('Email is already associated with an account.'));
     } else {
-      next(error);
+        next(error);
     }
 });
 
