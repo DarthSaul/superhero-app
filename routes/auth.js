fb739876c49c931c7238f55b0cee1fccb0b02ce5
Saul Graves <graves.saul@gmail.com>
2021-03-18 20:54:05 -0400

    Config login after register

--- routes/auth.js
+++ routes/auth.js
@@ -13,8 +13,11 @@ router.post('/register', wrapAsync(async (req, res) => {
         const { email, username, password } = req.body;
         const newUser = new User({email, username});
         const registerUser = await User.register(newUser, password);
-        req.flash("success", `Welcome, ${registerUser.username}!`);
-        res.redirect('/heroes')
+        req.login(registerUser, err => {
+            if (err) return next(err);
+            req.flash("success", `Welcome, ${registerUser.username}!`)
+            res.redirect('/heroes');
+        })
     } catch (error) {
         req.flash("error", error.message)
         res.redirect('/register')
