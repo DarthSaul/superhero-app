85bf9ab1ce9294409b9dbd08450962ff3cbc33fa
Saul Graves <graves.saul@gmail.com>
2021-03-30 14:25:02 -0400

    Map popups

--- public/js/showMap.js
+++ public/js/showMap.js
@@ -3,12 +3,23 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: geometry.coordinates, // starting position [lng, lat]
+    center: team.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 }); 
 
-const marker1 = new mapboxgl.Marker()
-.setLngLat(geometry.coordinates)
-.addTo(map);
-
 map.addControl(new mapboxgl.NavigationControl());
+
+const popup = new mapboxgl.Popup()
+    .setHTML(
+        `
+        <div id="map-popup">
+            <strong>${team.name}</strong>
+            <p>${team.hqLocation}</p>
+        </div>
+        `
+    );
+
+const marker1 = new mapboxgl.Marker()
+    .setLngLat(team.geometry.coordinates)
+    .setPopup(popup)
+    .addTo(map);
