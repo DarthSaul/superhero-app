39218f84f3e821cf3009c7d093390cb31783d9ab
Saul Graves <graves.saul@gmail.com>
2021-03-31 01:30:44 -0400

    Series display styles

--- controllers/search.js
+++ controllers/search.js
@@ -7,6 +7,15 @@ module.exports.findCharacter = wrapAsync(async(req, res) => {
     const url = buildMarvelApiRoute('/characters', queries);
     const { data } = await api.get(url)
     const { results } = data.data;
-    console.log(results[0])
+    // console.log(results[0])
     res.render('search/index', { results })
+})
+
+module.exports.findSeries = wrapAsync(async(req, res) => {
+    const { id } = req.params;
+    const url = buildMarvelApiRoute(`/characters/${id}/series`);
+    const { data } = await api.get(url)
+    const { results } = data.data;
+    console.log(results[0])
+    res.render('search/showSeries', { results })
 })
